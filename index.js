cd842b42b78df3c8bc9c9487198a2d345bbe5234
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-18 21:12:27 +0900

    support for binary format (bson) is added.

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ See the accompanying LICENSE file for terms.
 'use strict';
 
 const fs = require('fs');
+const bson = require('bson');
+const BSON = new bson.BSONPure.BSON();
+
 const initialValue = {};
+
+const JB_JSON = 'j';
+const JB_BSON = 'b';
 
 module.exports = {};
 
@@ -16,74 +22,88 @@ module.exports.initialValue =
 
 module.exports.filed =
   function( filePath, processOfFiledJson){
+    filedCore( filePath, processOfFiledJson, JB_JSON );
+  };
 
-    if(filePath == null){
-      throw new JsonFiledError( 'Path to file object is needed.' );
-    }
-
-    fs.open(
-      filePath,
-      'r+',
-      (err,fd) => {
-
-        if( ! err ){
-          //file to read exists.
-
-          //read from file and process
-          fs.readFile(
-            fd,
-            'utf8',
-            (err, data) => {
-              fs.close(fd);
-              if (err) throw new JsonFiledError('IOError Failed to read file.');
-
-              apply(
-                processOfFiledJson,
-                JSON.parse( data ),
-                filePath,
-                function(){} // no need to close filePath
-              );
-            }
-          );
-
-        }else{
-          //file to read does not exists.
-
-          //create file with initial value and process json.
-          fs.open(
-            filePath,
-            'w+',
-            (err,fd) => {
-
-              if(err) throw new JsonFiledError('IOError Failed to create file.') ;
-
-              //save to file and apply process.
-              save(
-                initialValue,
-                fd,
-                function(){
-                  //apply process
-                  apply(
-                    processOfFiledJson,
-                    initialValue,
-                    fd,
-                    function(){// close descriptor.
-                      fs.close( fd );
-                    }
-                  );
-                }
-              );
-
-            }
-          );
-        }
-      }
-    )
+module.exports.bFiled =
+  function( filePath, processOfFiledBson){
+    filedCore( filePath, processOfFiledBson, JB_BSON );
   };
 
 
+function filedCore( filePath, processOfFiledJson, jb){
+
+  if(filePath == null){
+    throw new JsonFiledError( 'Path to file object is needed.' );
+  }
+
+  fs.open(
+    filePath,
+    'r+',
+    (err,fd) => {
+
+      if( ! err ){
+        //file to read exists.
+
+        //read from file and process
+        fs.readFile(
+          fd,
+          encoding(jb),
+          (err, data) => {
+
+            fs.close(fd);
+            if (err) throw new JsonFiledError('IOError Failed to read file.');
+
+            apply(
+              processOfFiledJson,
+              decode( data, jb ),
+              filePath,
+              function(){}, // no need to close filePath
+              jb
+            );
+          }
+        );
+
+      }else{
+        //file to read does not exists.
+
+        //create file with initial value and process json.
+        fs.open(
+          filePath,
+          'w+',
+          (err,fd) => {
+
+            if(err) throw new JsonFiledError('IOError Failed to create file.') ;
+
+            //save to file and apply process.
+            save(
+              initialValue,
+              fd,
+              function(){
+                //apply process
+                apply(
+                  processOfFiledJson,
+                  initialValue,
+                  fd,
+                  function(){// close descriptor.
+                    fs.close( fd );
+                  },
+                  jb
+                );
+              },
+              jb
+            );
+
+          }
+        );
+      }
+    }
+  )
+};
+
+
 //apply process function to json.
-function apply( process, json, file, afterApply){
+function apply( process, json, file, afterApply, jb ){
 
   var result = process(json);
 
@@ -92,7 +112,8 @@ function apply( process, json, file, afterApply){
     save(
       result,
       file,
-      afterApply //executed after saved.
+      afterApply, //executed after saved.
+      jb
     );
 
   }else{
@@ -103,16 +124,34 @@ function apply( process, json, file, afterApply){
 }
 
 //file can be either of file path and descriptor
-function save( data, file, afterSaved){
+function save( data, file, afterSaved, jb ){
   fs.writeFile(
     file,
-    JSON.stringify(data),
-    'utf8',
+    encode(data,jb),
+    encoding(jb),
     (err)=>{
       if (err) throw new JsonFiledError('IOError failed to save json');
       afterSaved();//file is closed in afterSaved, if needed.
     }
   );
+}
+
+function encoding( jb ){
+  if( jb == JB_BSON ) return null;
+  else if( jb == JB_JSON ) return 'utf8';
+  else throw new JsonFiledError('encoding: jb must be JSON or BSON');
+}
+
+function decode( obj, jb ){
+  if( jb == JB_BSON ) return BSON.deserialize(obj);
+  else if ( jb == JB_JSON ) return JSON.parse(obj);
+  else throw new JsonFiledError('decode: jb must be JSON or BSON');
+}
+
+function encode( obj, jb ){
+  if( jb == JB_BSON ) return BSON.serialize( obj, false, true, false);
+  else if( jb == JB_JSON ) return JSON.stringify( obj );
+  else throw new JsonFiledError('encode: jb must be JSON or BSON');
 }
 
 function JsonFiledError(msg){
