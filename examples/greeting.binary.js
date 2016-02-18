a981668e5032a1c33da5a2130a60822f43e4c93e
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-18 21:19:01 +0900

    fix mistaken comment in example

--- examples/greeting.binary.js
+++ examples/greeting.binary.js
@@ -1,5 +1,5 @@
 var jf = require('json.filed');
 
 jf.bFiled('./data.bson',function(bson) {
-  return {msg: 'hello world.'}; // write 1st greeting to data.json
+  return {msg: 'hello world.'}; // write 1st greeting to data.bson
 });
