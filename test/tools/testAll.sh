d3aaf015eb94453075a8ebf94e082b078f198dfa
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-20 22:12:45 +0900

    fix bug in testAll.sh and add new test.

--- test/tools/testAll.sh
+++ test/tools/testAll.sh
@@ -2,6 +2,7 @@ mocha test/unit/test.js
 mocha test/unit/test_bson.js
 mocha test/unit/testChain.js
 mocha test/unit/testChain_more.js
+mocha test/unit/testChain_more_2.js
 mocha test/unit/testChainWithPass.js
 mocha test/unit/testNested.js
-sh rmTestJsonFile.sh
+sh test/tools/rmTestJsonFile.sh
