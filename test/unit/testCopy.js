a3f4f34b0f09dff3b2a1cbc628a127f311a1c925
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-21 09:42:21 +0900

    add test case to testCopy.js

--- test/unit/testCopy.js
+++ test/unit/testCopy.js
@@ -5,7 +5,7 @@ var jf = require('../../'),
     fs = require('fs');
 
 const testPath_1 = './' + Math.random() + '.json';
-const testPath_2 = './' + Math.random() + '.json'
+const testPath_2 = './' + Math.random() + '.json';
 
 var testValue = { msg: Math.random().toString() };
 
@@ -33,4 +33,38 @@ describe('Copy functions ', function () {
       },
       1000);
   });
+
+});
+
+const testPath_3 = './' + Math.random() + '.json';
+const testPath_4 = './' + Math.random() + '.json';
+
+describe('Copy functions ', function () {
+  it('should create multiple copied file', function (done) {
+
+    jf.filed( testPath_1 )
+    .copy(
+      function(obj, filePath){
+        return [ testPath_3, testPath_4 ];
+      }
+    ).exec();
+
+    setTimeout(
+      function(){
+        jf.filed( testPath_3 )
+        .pass( function( o ) {
+          expect( o ).to.eql( testValue );
+
+          jf.filed( testPath_4 )
+          .pass( function( o ) {
+            expect( o ).to.eql( testValue );
+            done();
+            
+          }).exec();
+
+        }).exec();
+      },
+      1000);
+  });
+
 });
