5c6999f8e2a1a2db9ac12d0c40cc8c91ba9bb4a4
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-23 00:04:12 +0900

    Commit missed fix

--- examples/greeting3.js
+++ examples/greeting3.js
@@ -1,6 +1,7 @@
 var jf = require('json.filed');
 
-jf.filed('./data.json').io( function(json) {
+jf.filed('./data.json')
+.io( function(json) {
   console.log(json.msg);// previous wrote greeting, 'good afternoon'
   json.msg = 'good night world.';
   // return json;
