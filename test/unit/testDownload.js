6e9e6a19d841e08e3be1ad6ec716b714b212df26
Tomohito Nakayama <tmnk@nysalor.local>
2016-02-28 08:11:49 +0900

    switch from github server to local server used in testDownload.js

--- test/unit/testDownload.js
+++ test/unit/testDownload.js
@@ -2,26 +2,24 @@
 
 var jf = require('../../'),
     expect    = require('chai').expect,
-    fs = require('fs');
+    http = require('http');
 
 let testPath = './' + Math.random() + '.json';
+let testValue = { msg: "hello world" };
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end( JSON.stringify( testValue ) );
+}).listen(8080);
 
 describe('Download function ', function () {
   this.timeout(10000);
   it('should download JSON from internet.', function (done) {
 
-    jf.download(
-      {
-        method: "GET",
-        uri: 'https://api.github.com/repos/7k8m/json.filed/commits/076aff7302cae3046955de13af41b1be90f41f03',
-        headers: {
-          'User-Agent': 'jsonfiled'
-        }
-      },
-      testPath )
+    jf.download( 'http://localhost:8080', testPath )
     .pass(
-      function(obj, filePath){
-        expect(obj.commit.author.name).to.eql( "Tomohito Nakayama" );
+      function( obj, filePath ){
+        expect( obj ).to.eql( testValue );
         done();
       }
     ).exec();
