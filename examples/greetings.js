2beb9c183709725e8a187f71cddd29f3ed170747
Tomohito Nakayama <tmnk@nysalor.local>
2016-03-01 21:39:05 +0900

    missed to commit

--- examples/greetings.js
+++ examples/greetings.js
@@ -1,4 +1,4 @@
 var jf = require('json.filed');
 
 jf.filed(['./hello.json','./😄.json'])
-.io( ( obj,filePath ) => { msg: filePath } }).exec(); // write 1st greeting to data.json
+.io( ( obj,filePath ) => { msg: filePath } ).exec(); // write 1st greeting to data.json
