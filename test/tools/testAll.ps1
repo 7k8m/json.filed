f73f6e3544c731fd786cc54c7b26e39a43e4cc8a
Tomohito Nakayama <tomonaka@basil.ocn.ne.jp>
2016-03-09 11:52:12 +0900

    script for Windows is updated.

--- test/tools/testAll.ps1
+++ test/tools/testAll.ps1
@@ -26,4 +26,6 @@ mocha test/unit/testParallel_2.js
 mocha test/unit/testFileUrl.js
 mocha test/unit/writeonlyJson.js
 mocha test/unit/readonlyJson.js
+mocha test/unit/testCollect.js
+mocha test/unit/testCollect_more.js
 test/tools/rmTestJsonFile.ps1
