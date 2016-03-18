717e344dfa02771c6ce5f4199034b232927ce866
Tomohito Nakayama <tmnk@nysalor.local>
2016-03-19 00:24:31 +0900

    Confirm JSON is ignored after stop Receiving listner called.

--- test/unit/testEvent.js
+++ test/unit/testEvent.js
@@ -7,6 +7,7 @@ var jf = require('../../'),
 
 const obj1 = { msg: 'This is 1' };
 const obj2 = { msg: 'This is 2' };
+const objToIgnore = { msg: 'ignored' };
 
 describe('Event executer ', function () {
   it('works without error', function (done) {
@@ -23,6 +24,8 @@ describe('Event executer ', function () {
             receiveListner( obj2 );
             stopListener();
 
+            receiveListner( objToIgnore );
+
           }, 200 );
         }, 200 );
 
@@ -34,6 +37,7 @@ describe('Event executer ', function () {
       function(obj){
         expect( obj[0] ).to.be.eql( obj1 );
         expect( obj[1] ).to.be.eql( obj2 );
+        expect( obj.length ).to.be.equal( 2 );
         done();
       },
       './' + Math.random() + '.json',
