56974fd2763eb750e1de09f6108736f39a73d403
Tomohito Nakayama <tmnk@nysalor.local>
2016-03-19 20:19:11 +0900

    update map reduce like

--- examples/mapReduce_Like.js
+++ examples/mapReduce_Like.js
@@ -11,52 +11,59 @@ jf.filed( './msg01.json' )
     .write( { msg: 'Hello json.filed Goodbye json.filed' } )
     .pass(
       () =>{
+
         jf.filed([ './msg01.json', './msg02.json' ])
-        .calledback( ( msgObj, filePath, callback ) => {
+        .pass( ( msgObj, filePath ) => {
             //Map phase
             for( let word of msgObj.msg.split(' ')){
-              callback( { key : word, value: 1 }, './' + count + '.json');
+              shuffler.receive( { key : word, value: 1 });
               count ++;
             }
-            callback();
-
           }
-        )
-        .filter( (obj,filePath) => {
-          let basename = path.basename(filePath)
-          return  basename != 'msg01.json' &&
-                  basename != 'msg02.json'
-          }
-        )
-        .collect( ( kvArray ) =>{
-          var obj = {};
-          for( let kv of kvArray ){
-            if( ! obj[ kv.key ] ) obj[ kv.key ] = [];
-            obj[kv.key].push(kv.value);
-          }
-
-          return obj;
-
-        },'./collect.json' )
-        .calledback( (collectedObj, filePath, callback) => {
-          for( let word in collectedObj ){
-            callback({ key: word, value: collectedObj[word] }, './' + word + '.json' );
-          }
-          callback();
-        })
-        .filter( (obj, filePath) => path.basename(filePath) != 'collect.json'  )
-        .io( obj => {
-            //Reduce phase
-              var sum = 0;
-              for( let c of obj.value ){
-                sum = sum + c;
-              }
-              return { key: obj.key, value: sum };
-          }
-        )
-        .collect( obj => obj, './answer.json' )
-        .exec();
+        ).collect(
+          () => { shuffler.stop(); },
+          './collect_1.json'
+        ).exec();
       }
     ).exec();
   }
 ).exec();
+
+var c = 0;
+let shuffler =
+  jf.event(
+    ()=>{},
+    () => {
+      c ++;
+      return './' + c + '.json' } )
+  .collect( ( kvArray ) => {
+      var obj = {};
+      for( let kv of kvArray ){
+        if( ! obj[ kv.key ] ) obj[ kv.key ] = [];
+        obj[kv.key].push(kv.value);
+      }
+      return obj; },
+    './collect_2.json' )
+  .pass( (collectedObj, filePath ) => {
+    for( let word in collectedObj ){
+      reducer.receive( { key: word, value: collectedObj[word] });
+    }
+    reducer.stop();
+  })
+  .exec();
+
+let reducer =
+  jf.event(
+    () => {},
+    ( obj ) => { return './' + obj.key + '.json'} )
+  .io( obj => {
+      //Reduce phase
+        var sum = 0;
+        for( let c of obj.value ){
+          sum = sum + c;
+        }
+        return { key: obj.key, value: sum };
+    }
+  )
+  .collect( obj => obj, './answer.json' )
+  .exec();
