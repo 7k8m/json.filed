ceea4868f40ac442fc2c949a5b1a11ed1b99f2cc
Tomohito Nakayama <tmnk@nysalor.local>
2016-05-10 20:54:11 +0900

    Add repl feature

---
+++ repl.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+/*
+Copyright (c) 2016, Tomohito Nakayama. All rights reserved.
+Copyrights licensed under the New BSD License.
+See the accompanying LICENSE file for terms.
+*/
+
+'use strict';
+
+const jf = require('./index.js')
+const repl = require('repl');
+
+repl.start('>').context.jf = jf;
